faf3ff656936f9bdb03aa1f102980f04583041bf
Daemon <vitorevil12@gmail.com>
2024-09-24 12:15:33 -0300

    Update TemperatureAnalyzerTest.java
    test: add test for almost all days above average temperature

--- src/test/java/TemperatureAnalyzerTest.java
+++ src/test/java/TemperatureAnalyzerTest.java
@@ -13,4 +13,10 @@ public class TemperatureAnalyzerTest {
         int[] temperatures = {21, 10, 13, 34, 30, 21, 34};
         assertEquals(3, TemperatureAnalyzer.countDaysAboveAverage(temperatures));
     }
+
+    @Test
+    void testValidInputAlmostAllAboveAverage() {
+        int[] temperatures = {2, 2, 2, 2, 2, 2, 1};
+        assertEquals(6, TemperatureAnalyzer.countDaysAboveAverage(temperatures));
+    }
 }
